7d3aebed91af50e4267ce449dd561a2e8eeccc75
Shannah Santucci <shannah.santucci@gmail.com>
2023-08-30 15:06:19 +0200

    Add orbital controls and extract dropdown function.

--- hollo-web/main.js
+++ hollo-web/main.js
@@ -1,20 +1,23 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { dropdown } from './src/dropdown.js';
 import { loadObj } from './src/objLoader.js';
+import { createSphere } from './src/sphere.js';
+
 
 let cube, renderer, scene, camera, controls, glasses;
 
 init();
 loadObjects();
+loadMenu()
 animate();
 
 function init() {
     document.addEventListener('contextmenu', event => event.preventDefault());
     window.addEventListener('resize', onWindowResize)
 
-    dropdown()
-
     scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(5))
     scene.background = new THREE.Color( 0xffffff );
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -26,25 +29,22 @@ function init() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const offset = new THREE.Vector3(0, 0, 0)
+    scene.add(createSphere(cube, offset));
 
-    const buttons = document.querySelectorAll('#button0, #button1, #button2, #button3');
-    buttons.forEach((button, index) => {
-        button.addEventListener('click', function() {
-            onButtonClick(index)
-        }, false)
-    });
-
-    camera.position.z = 10;
+    camera.position.z = 15;
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
 }
 
 function loadObjects() {
-    const position = new THREE.Vector3(-4, 0, -4)
+    const position = new THREE.Vector3(0, 0, 0)
     const rotation = new THREE.Euler(-Math.PI/2, 0, 0)
-    loadObj("glasses", 0.1, position, rotation)
+    loadObj("glasses", 0.12, position, rotation)
         .then((object) => {
             glasses = object
-            scene.add(glasses)
+            const offset = new THREE.Vector3(-5, 0, 0)
+            scene.add(createSphere(glasses, offset))
         })
         .catch((error) => {
             console.error(`Error while loading obj: ${error}`);
@@ -52,11 +52,12 @@ function loadObjects() {
 }
 
 function animate() {
-	requestAnimationFrame(animate);
+	requestAnimationFrame( animate );
 
-    glasses.rotation.z += 0.02;
+	glasses.rotation.x += 0.01;
+	glasses.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+	renderer.render( scene, camera );
 }
 
 function onButtonClick(index){
@@ -68,4 +69,14 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function loadMenu() {
+    dropdown()
+    const buttons = document.querySelectorAll('#button0, #button1, #button2, #button3');
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', function() {
+            onButtonClick(index)
+        }, false)
+    });
 }

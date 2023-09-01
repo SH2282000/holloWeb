25be39cd11da3fabd744a4ff1bf0be3e9e861cf8
Shannah Santucci <shannah.santucci@gmail.com>
2023-09-01 17:47:29 +0200

    Add closing function in menu.

--- hollo-web/src/dropdown.js
+++ hollo-web/src/dropdown.js
@@ -6,6 +6,11 @@ export function dropdown(idButton) {
   // add click event to dropdown button
   dropdownBtn.addEventListener("click", () => {
     dropdownCaret.classList.toggle("arrow-rotate");
+    if (idButton == 0) {
+      for (const x of Array(2).keys()) {
+        closeEverything(x + 1)
+      }
+    }
     dropdownContent.classList.toggle("menu-open");
     dropdownBtn.setAttribute(
       "aria-expanded",
@@ -14,3 +19,12 @@ export function dropdown(idButton) {
   });
 }
 
+function closeEverything(idButton) {
+  const dropdownBtn = document.getElementById(`dropdown-btn${idButton}`);
+  const dropdownCaret = document.getElementById(`arrow${idButton}`);
+  const dropdownContent = document.getElementById(`dropdown-content${idButton}`);
+
+  dropdownCaret.classList.remove("arrow-rotate");
+  dropdownContent.classList.remove("menu-open");
+  dropdownBtn.setAttribute("aria-expanded", "false");
+}

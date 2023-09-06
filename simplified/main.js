bd5e0a56eeb673f40fa3a8c033d27add5a11863a
Shannah Santucci <shannah.santucci@gmail.com>
2023-09-06 10:51:08 +0200

    Add simplified version.

--- simplified/main.js
+++ simplified/main.js
@@ -1,24 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { dropdown } from './src/dropdown.js';
+import { btnVideosPress } from './src/video.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+let renderer, scene, camera, controls, glasses, speed = 0.1;
+
+init();
+
+
+function init() {
+    // Loading Scene
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    loadMenu()
+    btnVideosPress()
+}
+
+function loadMenu() {
+    const numOfButtons = 8
+    for (const x of Array(numOfButtons).keys()) {
+        dropdown(x, numOfButtons)
+    }
+}
---
+++ simplified/src/dropdown.js
@@ -0,0 +1,35 @@
+import { changeVideo } from "./video";
+
+export function dropdown(idButton, numOfButtons) {
+  const dropdownBtn = document.getElementById(`dropdown-btn${idButton}`);
+  const dropdownCaret = document.getElementById(`arrow${idButton}`);
+  const dropdownContent = document.getElementById(`dropdown-content${idButton}`);
+
+  // add click event to dropdown button
+  dropdownBtn.addEventListener("click", () => {
+    dropdownCaret.classList.toggle("arrow-rotate");
+    if (idButton == 0) {
+      changeVideo("chaussures3")
+      for (const x of Array(numOfButtons - 1).keys()) {
+        closeEverything(x + 1)
+      }
+    } else {
+      closeEverything(0)
+    }
+    dropdownContent.classList.toggle("menu-open");
+    dropdownBtn.setAttribute(
+      "aria-expanded",
+      dropdownBtn.getAttribute("aria-expanded") === "true" ? "false" : "true"
+    );
+  });
+}
+
+function closeEverything(idButton) {
+  const dropdownBtn = document.getElementById(`dropdown-btn${idButton}`);
+  const dropdownCaret = document.getElementById(`arrow${idButton}`);
+  const dropdownContent = document.getElementById(`dropdown-content${idButton}`);
+
+  dropdownCaret.classList.remove("arrow-rotate");
+  dropdownContent.classList.remove("menu-open");
+  dropdownBtn.setAttribute("aria-expanded", "false");
+}
